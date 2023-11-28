c6d61a22e11617f513c9b20f97ae805c95d21121
HoangKhang269 <nguyenkhang099799@gmail.com>
2023-11-28 21:29:07 +0700

    update demo 28.11 9:28

---
+++ WebDNC/Controllers/KhoaCNTT/HomeCNTTController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDNC.Controllers.KhoaCNTT
+{
+    public class HomeCNTTController : Controller
+    {
+        // GET: HomeCNTT
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
